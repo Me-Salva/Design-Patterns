4ed0974d8be44da5b9535a01c7c2b50e1f479f40
Guilherme Diogo <up201806340@fe.up.pt>
2021-12-19 20:07:58 +0000

    Old Progress

---
+++ src/main/java/StringCaseChanger.java
@@ -0,0 +1,16 @@
+public class StringCaseChanger implements StringTransformer {
+    @Override
+    public void execute(StringDrink drink) {
+        StringBuilder str = new StringBuilder(drink.getText());
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isLowerCase(str.charAt(i))) {
+                str.setCharAt(i, Character.toUpperCase(str.charAt(i)));
+            } else {
+                str.setCharAt(i, Character.toLowerCase(str.charAt(i)));
+            }
+
+        }
+        drink.setText(str.toString());
+
+    }
+}
